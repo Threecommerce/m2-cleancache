d474c52d115e2768a524ab8575ffac657223491f
Gianluca <gporta@gianlucaporta.it>
2024-11-13 11:22:50 +0100

    first commit

---
+++ Controller/Adminhtml/Clean/Cache.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Gp\CleanCache\Controller\Adminhtml\Clean;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\Message\ManagerInterface;
+
+class Cache extends Action
+{
+    protected $resultFactory;
+    protected $messageManager;
+
+    public function __construct(
+        Context          $context,
+        ManagerInterface $messageManager,
+        ResultFactory    $resultFactory
+    )
+    {
+        $this->resultFactory = $resultFactory;
+        $this->messageManager = $messageManager;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        exec('php bin/magento c:f');
+        $this->messageManager->addSuccess(__("Successfully cleaned caches."));
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setUrl($this->_redirect->getRefererUrl());
+    }
+
+    protected function _isAllowed()
+    {
+        return true;
+    }
+}
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Gp_CleanCache',
+    __DIR__
+);
